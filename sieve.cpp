9a23980faab94f5f7c3f264c69f1ac499fbb4e16
MD Tausiful Bari <52565015+MdTausifulBari@users.noreply.github.com>
2020-02-03 20:24:17 +0600

    Add files via upload

---
+++ sieve.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool mark[10000005];
+int prime[10000005];
+
+void sieve(int n)
+{
+    mark[0] = mark[1] = 1;
+
+    int limit = sqrt(n)+1;
+
+    for(int i = 4; i <= n; i += 2)
+        mark[i] = 1;
+
+    for(int i = 3; i <= limit; i += 2)
+    {
+        if(mark[i] == 0)
+        {
+            for(int j = i*i; j <= n; j += 2*i)
+                mark[j] = 1;
+        }
+    }
+
+    int k = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        if(mark[i] != 1)
+        {
+            prime[k] = i;
+            k++;
+        }
+    }
+}
+
+int main()
+{
+    sieve(10000);
+
+    int num;
+    cin >> num;
+
+    for(int i = 0; i < num; i++)
+        cout << prime[i] << endl;
+    cout << endl;
+
+    return 0;
+}
+
